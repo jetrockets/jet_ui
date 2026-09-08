a5a80d09f744e07973b08366bad4e0128b09768f
RPA <ingrepa0214@gmail.com>
2026-09-08 13:54:08 -0500

    refactor(modal): deprecate Modal in favor of Dialog(position: :center)
    JetUi::Modal::Component and its Header/Body/Footer sub-components are
    now thin shims delegating to JetUi::Dialog::Component and friends.
    Existing jet_ui.modal(...) calls keep working and emit an
    ActiveSupport::Deprecation warning. Will be removed in the next major
    version. Part of #12.

--- app/components/jet_ui/modal/body_component.rb
+++ app/components/jet_ui/modal/body_component.rb
@@ -2,20 +2,8 @@
 
 module JetUi
   module Modal
-    class BodyComponent < JetUi::BaseComponent
-      def initialize(**options)
-        @options = options
-      end
-
-      def call
-        content_tag :div, content, class: classes, **@options.except(:class)
-      end
-
-      private
-
-      def classes
-        class_names('modal__body', @options[:class])
-      end
+    # @deprecated Use {JetUi::Dialog::BodyComponent} instead.
+    class BodyComponent < JetUi::Dialog::BodyComponent
     end
   end
 end
